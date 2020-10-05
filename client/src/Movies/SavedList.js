68adcf8a0b9d7d56a743573d3764796f1f0f5f3c
ORiveraJr84 <oriverajr84@gmail.com>
2020-10-05 02:16:49 -0400

    No meaningful commits. Sorry.

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { NavLink, useHistory } from "react-router-dom";
 
 export default function SavedList(props) {
+  let history = useHistory();
+
+  const goHome = () => {
+    history.push("/");
+  };
+
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
+      {props.list.map((movie) => (
+        <NavLink className="saved-movie">{movie.title}</NavLink>
       ))}
-      <div className="home-button">Home</div>
+      <div className="home-button" onClick={() => goHome()}>
+        Home
+      </div>
     </div>
   );
 }
